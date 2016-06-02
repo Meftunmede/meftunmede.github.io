a29224ad642a1f6afede9f42f7a0afd3001d5f46
Meftun MEDE <meftun_hh@hotmail.de>
2016-06-03 00:26:02 +0200

    Add files via upload

---
+++ javascripts/social-js-right.js
@@ -0,0 +1,20 @@
+
+jQuery(document).ready(function(){ jQuery("#facebook_box").hover(function(){ 
+
+jQuery(this).stop(true,false).animate({left:-296}, 500); },function(){ 
+
+jQuery("#facebook_box").stop(true,false).animate({left: 0}, 500); });   
+
+
+jQuery("#twitter_box").hover(function(){ 
+
+jQuery(this).stop(true,false).animate({left:-296}, 500); },function(){ 
+
+jQuery("#twitter_box").stop(true,false).animate({left: 0}, 500); }); 
+
+jQuery("#google_box").hover(function(){ 
+
+jQuery(this).stop(true,false).animate({left:-296}, 500); },function(){ 
+
+jQuery("#google_box").stop(true,false).animate({left: 0}, 500); }); });
+
